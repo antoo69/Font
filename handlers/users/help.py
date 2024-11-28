ee91e9ade0fd2f040f95d7289cb98b19a508c06a
antoo69 <antobarry69@gmail.com>
2024-11-28 17:34:22 +0700

    Update help.py

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -6,7 +6,7 @@ from loader import dp
 
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
-    text = ("Buyruqlar: ",
+    text = ("Perintah: ",
             "/start - to start the bot",
             "/help - to get help")
     
