dd6376ffaadfefe87661f77ff3d12d5f02a11747
antoo69 <antobarry69@gmail.com>
2024-11-28 17:28:14 +0700

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -9,12 +9,12 @@ from utils.db_api.bot_users import create_bot_user
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    txt1 = f"<i>Dear</i> <b>{message.from_user.full_name},</b>"
-    txt1 += "\nWelcome to the bot"
-    txt1 += "\n\nWith this bot, you can convert your text to different fonts."
+    txt1 = f"<i>Halo</i> <b>{message.from_user.full_name},</b>"
+    txt1 += "\nWelcome to ꜰ໑ʀᴆɪ Changer bot"
+    txt1 += "\n\nDengan bot ini, anda bisa mengubah text anda menjadi font yang berbeda."
     await message.answer(text=txt1)
-    txt2 = "<i>Please, select a font from the</i> <b>📋 Fonts list 📝 </b> <i>section, \
-        \nor click</i> <b>☑️ Apply all fonts ✅</b><i> button to use all fonts at once</i>"
+    txt2 = "<i>Silahkan pilih font </i> <b>📋 Daftar Font 📝 </b> <i>section, \
+        \nor click</i> <b>☑️ Setujui Semua Font ✅</b><i> tombol untuk menggunakan semua font sekaligus</i>"
     await message.answer(text=txt2, reply_markup=menu_button)
 
     # User'ni database'ga saqlaymiz
